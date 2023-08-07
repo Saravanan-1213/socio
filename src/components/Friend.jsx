85c18aab17fe4c4e2cab870d43540133d6c6f37f
Saravanan-1213mm <saravanansekar033@gmail.com>
2023-08-07 16:42:39 +0530

    api

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `https://backend-media-awft.onrender.com/users/${_id}/${friendId}`,
+      `backend-media-awft.onrender.com/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
